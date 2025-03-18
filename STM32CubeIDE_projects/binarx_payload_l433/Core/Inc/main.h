621bb444e228b81a46c35e92267f8984d6a1ad09
Will Blackie <292823B@curtin.edu.au>
2025-03-18 11:09:59 +0800

    added the L433 to the branch

---
+++ STM32CubeIDE_projects/binarx_payload_l433/Core/Inc/main.h
@@ -0,0 +1,122 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32l4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <string.h> // Include the String data type
+#include <stdio.h> // sprintf()
+#include <stdbool.h> // Include the String data type
+#include <stdint.h> // Include the uint data types (uint8_t)
+//#include "peripheral.h"
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define FC_GPIO_1_Pin GPIO_PIN_13
+#define FC_GPIO_1_GPIO_Port GPIOC
+#define FC_GPIO_4_Pin GPIO_PIN_0
+#define FC_GPIO_4_GPIO_Port GPIOA
+#define SPI1_CS_Pin GPIO_PIN_2
+#define SPI1_CS_GPIO_Port GPIOA
+#define SPI1_CS_EXTI_IRQn EXTI2_IRQn
+#define FC_GPIO_3_Pin GPIO_PIN_3
+#define FC_GPIO_3_GPIO_Port GPIOA
+#define SENSOR_ADC_1_Pin GPIO_PIN_4
+#define SENSOR_ADC_1_GPIO_Port GPIOA
+#define SENSOR_ADC_2_Pin GPIO_PIN_5
+#define SENSOR_ADC_2_GPIO_Port GPIOA
+#define SENSOR_ADC_3_Pin GPIO_PIN_0
+#define SENSOR_ADC_3_GPIO_Port GPIOB
+#define SENSOR_ADC_4_Pin GPIO_PIN_1
+#define SENSOR_ADC_4_GPIO_Port GPIOB
+#define FC_GPIO_2_Pin GPIO_PIN_2
+#define FC_GPIO_2_GPIO_Port GPIOB
+#define DEBUG_USART_TX_Pin GPIO_PIN_10
+#define DEBUG_USART_TX_GPIO_Port GPIOB
+#define DEBUG_USART_RX_Pin GPIO_PIN_11
+#define DEBUG_USART_RX_GPIO_Port GPIOB
+#define SENSOR_SPI_CS_Pin GPIO_PIN_12
+#define SENSOR_SPI_CS_GPIO_Port GPIOB
+#define SENSOR_SPI_CS_EXTI_IRQn EXTI15_10_IRQn
+#define SENSOR_SPI_SCK_Pin GPIO_PIN_13
+#define SENSOR_SPI_SCK_GPIO_Port GPIOB
+#define SENSOR_SPI_POCI_Pin GPIO_PIN_14
+#define SENSOR_SPI_POCI_GPIO_Port GPIOB
+#define SENSOR_SPI_PICO_Pin GPIO_PIN_15
+#define SENSOR_SPI_PICO_GPIO_Port GPIOB
+#define SENSOR_USART_TX_Pin GPIO_PIN_9
+#define SENSOR_USART_TX_GPIO_Port GPIOA
+#define SENSOR_USART_RX_Pin GPIO_PIN_10
+#define SENSOR_USART_RX_GPIO_Port GPIOA
+#define SENSOR_GPIO_3_Pin GPIO_PIN_12
+#define SENSOR_GPIO_3_GPIO_Port GPIOA
+#define SENSOR_GPIO_2_Pin GPIO_PIN_5
+#define SENSOR_GPIO_2_GPIO_Port GPIOB
+#define SENSOR_I2C_SCL_Pin GPIO_PIN_6
+#define SENSOR_I2C_SCL_GPIO_Port GPIOB
+#define SENSOR_I2C_SDA_Pin GPIO_PIN_7
+#define SENSOR_I2C_SDA_GPIO_Port GPIOB
+#define SENSOR_GPIO_1_Pin GPIO_PIN_8
+#define SENSOR_GPIO_1_GPIO_Port GPIOB
+#define SENSOR_GPIO_INT_Pin GPIO_PIN_9
+#define SENSOR_GPIO_INT_GPIO_Port GPIOB
+#define SENSOR_GPIO_INT_EXTI_IRQn EXTI9_5_IRQn
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ STM32CubeIDE_projects/binarx_payload_l433/peripheral_library/peripheral.c
@@ -0,0 +1,61 @@
+/**
+ * @file peripheral.c
+ * @author Will
+ * @brief Binar X Peripheral Source (Adapted from Emulator Libraries)
+ *
+ */
+
+#include "peripheral.h"
+
+void Transmit(uint8_t *data, uint16_t data_size) {
+    // Calculate number of packets
+    //uint16_t data_size = sizeof(data) / sizeof(data[0]);
+    uint16_t num_packets = data_size / kPacketLength;
+    // Add an extra packets if the division above has a reminder
+    if (data_size % kPacketLength != 0) {
+      num_packets++;
+    }
+
+    // calculate size of data transfered
+    bytes_to_send = num_packets * kPacketLength + kNumberOfBytesInHeader;
+
+    // Populate the header
+    buffer_data[0] = kSyncByte;
+    buffer_data[1] = data_size >> 8;
+    buffer_data[2] = data_size;
+
+    // fill the buffer with the data
+    if (data_size <= kMaxPayloadDataLength) {
+      for (uint16_t i = 0; i < data_size; i++) {
+        buffer_data[i + kNumberOfBytesInHeader] = data[i];
+      }
+    }
+
+    // TODO(tristan): ChipSelect does not change the behaviour. move this line
+    // to ChipSelectInterrupt()
+    
+}
+
+void TransmitCallBackInterrupt() {
+}
+
+
+void ChipSelectInterrupt(SPI_HandleTypeDef *hspi, bool kPayloadReadyToTransmit) {
+	if (kPayloadReadyToTransmit) {
+		HAL_SPI_Transmit(hspi, buffer_data, sizeof(buffer_data),1000);
+	}
+}
+
+/*
+bool TransmitCallBackInterrupt(SPI_HandleTypeDef *hspi1, bool kPayloadReadyToTransmit) {
+	if (kPayloadReadyToTransmit) {
+		HAL_SPI_Transmit_IT(hspi1, buffer_data, bytes_to_send);
+		kTransferComplete = true;
+	}
+	else
+	{
+		kTransferComplete = false;
+	}
+	return kTransferComplete;
+}
+*/
